3f73c65da8a5571667d7dd2fd004621e75a7355f
zolori <44702305+zolori@users.noreply.github.com>
2021-10-13 16:06:01 +0200

    v0.0.13-2a
    cochons sur roulette implantés

--- Assets/Scripts/EndBehaviour.cs
+++ Assets/Scripts/EndBehaviour.cs
@@ -108,12 +108,8 @@ public class EndBehaviour : MonoBehaviour
 
     public void UpdateText()
     {
-        
         moneyText.SetText("Money : " + playerMoney);    
         waveText.SetText("Wave : " + spawner.GetWave());
         lifeText.SetText("Life : " + playerLife);
-        
-        
-        
     }
 }
